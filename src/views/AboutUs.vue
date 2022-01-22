4c2aaf1ea1d94737ee6f495b626cd72e1e92e806
Carl Lin <83962541+carl0510@users.noreply.github.com>
2022-01-22 17:19:40 +0800

    remove hello world

---
+++ src/views/AboutUs.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="about-us-main">
+    <h1 class="band">Super Napkin is a fuzz-pop trio hailing from Taiwan.</h1>
+    <p class="band-desc">
+      As 80’s kids born in the Taipei Basin, the drone of motorbikes comprised
+      their early childhood ‘soundtrack’. So when they came of age in the
+      Soulseek 56k baud modem era, it made perfect sense for them to fall in
+      love with the equally droney sounds of Yo La Tengo, Sonic Youth, Eric's
+      Trip, The Microphones, and My Bloody Valentine.
+    </p>
+    <div class="photo">
+      <g-image src="~/assets/img/about.jpg" alt="super napkin" />
+    </div>
+    <ul class="link-area">
+      <a href="mailto:supernapkinsogood@gmail.com" class="hover-target">
+        <li class="link-area__item">
+          <span class="material-icons"> alternate_email </span>
+        </li>
+      </a>
+      <a href="https://www.facebook.com/SuperNapkin/" class="hover-target">
+        <li class="link-area__item">
+          <span class="material-icons"> facebook </span>
+        </li>
+      </a>
+      <a href="https://www.instagram.com/super_napkin/" class="hover-target">
+        <li class="link-area__item">
+          <span class="material-icons"> monochrome_photos </span>
+        </li>
+      </a>
+    </ul>
+  </main>
+</template>
