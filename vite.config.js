04e22172f268f6600e457115fe9e90d4f4b70d8f
Tanzi Carl <83962541+carl0510@users.noreply.github.com>
2022-02-09 10:18:23 +0800

    style about us

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import PurgeIcons from 'vite-plugin-purge-icons'
 import eslintPlugin from 'vite-plugin-eslint'
 import Components from 'unplugin-vue-components/vite'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
