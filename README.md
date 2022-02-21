9d74aada501a0cc56a671a15ee44366e70f4b707
Tanzi Carl <83962541+carl0510@users.noreply.github.com>
2022-02-21 10:44:18 +0800

    add i18n plugin

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 [vue-img-viewr](https://github.com/jekorx/vue-img-viewr)
 
 [vue3-lazyload](https://github.com/murongg/vue3-lazyload)
+
+[@intlify/vite-plugin-vue-i18n](https://github.com/intlify/bundle-tools/tree/main/packages/vite-plugin-vue-i18n)
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { fileURLToPath } from 'url'
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueI18n from '@intlify/vite-plugin-vue-i18n'
 import PurgeIcons from 'vite-plugin-purge-icons'
 import eslintPlugin from 'vite-plugin-eslint'
 import Components from 'unplugin-vue-components/vite'
@@ -9,6 +11,9 @@ import Components from 'unplugin-vue-components/vite'
 export default defineConfig({
     plugins: [
         vue(),
+        vueI18n({
+            include: path.resolve(__dirname, './path/to/src/locales/**'),
+        }),
         PurgeIcons({
             // globs for searching source file to analyze
             content: [
