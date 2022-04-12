7291dd4c30613510e5122e770f7b0991b6dddebb
Carl Lin <63276356+xllin0050@users.noreply.github.com>
2022-04-12 14:15:52 +0800

    add overflow-x

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
-    <div class="min-h-screen w-full dark:bg-neutral-900 dark:text-purple-200">
-        <ThemeSwitch />
-        <LangSwitch
-            v-if="route.name === 'About Us'"
-            @change-router="router.go(0)"
-        />
-        <NoGlitchTitle />
-        <AppNavbar :routes-list="routes" />
-        <SiteNavbar :routes-list="routes" />
-        <div class="mx-auto max-w-xs lg:max-w-4xl">
-            <!-- <router-view /> -->
-            <router-view v-slot="{ Component }">
-                <transition name="fade" mode="out-in">
-                    <component :is="Component" />
-                </transition>
-            </router-view>
-        </div>
-        <SiteFooter />
-        <ScrollToTop />
+  <div
+    class="min-h-screen w-screen overflow-x-hidden dark:bg-neutral-900 dark:text-purple-200"
+  >
+    <ThemeSwitch />
+    <LangSwitch
+      v-if="route.name === 'About Us'"
+      @change-router="router.go(0)"
+    />
+    <NoGlitchTitle />
+    <AppNavbar :routes-list="routes" />
+    <SiteNavbar :routes-list="routes" />
+    <div class="mx-auto max-w-xs lg:max-w-4xl">
+      <!-- <router-view /> -->
+      <router-view v-slot="{ Component }">
+        <transition name="fade" mode="out-in">
+          <component :is="Component" />
+        </transition>
+      </router-view>
     </div>
+    <SiteFooter />
+    <ScrollToTop />
+  </div>
 </template>
 <script setup>
 import { onMounted } from 'vue'
@@ -26,47 +28,46 @@ import { useRouter, useRoute } from 'vue-router'
 const router = useRouter()
 const route = useRoute()
 const routes = [
-    'Home',
-    'About Us',
-    'Discography',
-    'Gigs',
-    'Videos',
-    'Photos',
-    'Products',
+  'Home',
+  'About Us',
+  'Discography',
+  'Gigs',
+  'Videos',
+  'Photos',
+  'Products',
 ]
 
 onMounted(() => {
-    localStorage.setItem('theme', 'light')
+  localStorage.setItem('theme', 'light')
 
-    if (!localStorage.getItem('lang')) {
-        const zh = ['zh-tw', 'zh-cn', 'zh-hk']
-        if (
-            zh.includes(
-                (
-                    window.navigator.language ||
-                    window.navigator.browserLanguage
-                ).toLowerCase()
-            )
-        ) {
-            localStorage.setItem('lang', 'zh')
-        } else {
-            localStorage.setItem('lang', 'en')
-        }
+  if (!localStorage.getItem('lang')) {
+    const zh = ['zh-tw', 'zh-cn', 'zh-hk']
+    if (
+      zh.includes(
+        (
+          window.navigator.language || window.navigator.browserLanguage
+        ).toLowerCase()
+      )
+    ) {
+      localStorage.setItem('lang', 'zh')
+    } else {
+      localStorage.setItem('lang', 'en')
     }
+  }
 })
 </script>
 
 <style>
 body {
-    position: relative;
+  position: relative;
 }
 .fade-enter-active,
 .fade-leave-active {
-    transition: opacity 0.3s ease;
+  transition: opacity 0.3s ease;
 }
 
 .fade-enter-from,
 .fade-leave-to {
-    opacity: 0;
+  opacity: 0;
 }
 </style>
