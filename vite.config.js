1ba3c03c7fc197ddc352028ff09742939306c4e0
carl <carl@r54500u>
2022-04-13 20:51:58 +0800

    add ios 7

--- vite.config.js
+++ vite.config.js
@@ -9,30 +9,30 @@ import eslintPlugin from 'vite-plugin-eslint'
 import Components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        legacy({
-            targets: ['defaults', 'not IE 11'],
-        }),
-        vueI18n({
-            runtimeOnly: false,
-            include: path.resolve(__dirname, './src/language/**'),
-        }),
-        PurgeIcons({
-            content: ['**/*.vue'],
-            format: 'mjs',
-        }),
-        Components(),
-        eslintPlugin(),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-            'vue-i18n': 'vue-i18n/dist/vue-i18n.esm-bundler.js',
-        },
+  plugins: [
+    vue(),
+    legacy({
+      targets: ['defaults', 'iOS 7', 'not IE 11'],
+    }),
+    vueI18n({
+      runtimeOnly: false,
+      include: path.resolve(__dirname, './src/language/**'),
+    }),
+    PurgeIcons({
+      content: ['**/*.vue'],
+      format: 'mjs',
+    }),
+    Components(),
+    eslintPlugin(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'vue-i18n': 'vue-i18n/dist/vue-i18n.esm-bundler.js',
     },
-    server: {
-        host: '0.0.0.0',
-        port: 4000,
-    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 4000,
+  },
 })
