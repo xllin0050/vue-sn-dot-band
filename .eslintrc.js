d3908c7cf7bff28a3b652c0fe770b8e51cfebeaa
Carl Lin <63276356+xllin0050@users.noreply.github.com>
2022-06-01 16:08:48 +0800

    fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,22 @@
 module.exports = {
-    root: true,
-    env: {
-        es2021: true,
-        browser: true,
-        node: true,
-    },
-    extends: ['plugin:vue/vue3-strongly-recommended', 'prettier'],
-    parserOptions: {
-        ecmaVersion: 2021,
-        sourceType: 'module',
-    },
-    plugins: ['vue'],
-    rules: {
-        // override/add rules settings here, such as:
-        // 'prettier/prettier': ['error', { endOfLine: 'auto' }],
-        'vue/no-unused-vars': 'off',
-        'vue/require-default-prop': 'off',
-        'no-console': 'off',
-    },
+  root: true,
+  env: {
+    es2021: true,
+    browser: true,
+    node: true,
+  },
+  extends: ['plugin:vue/vue3-strongly-recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'vue/no-unused-vars': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/multi-word-component-names': 'off',
+    'no-console': 'off',
+  },
 }
