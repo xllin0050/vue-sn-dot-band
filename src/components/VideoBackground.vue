c16e4a4873947d34f5c28b08c78fd1aca8881dcb
Lin Xinliang <63276356+xllin0050@users.noreply.github.com>
2022-07-27 14:37:07 +0800

    Revert "Dev"

--- src/components/VideoBackground.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <video autoplay="true" loop class="min-w-full">
-    <source :src="videoUrl" type="video/mp4" />
-  </video>
-</template>
-<script setup>
-const videoUrl = new URL('../assets/SNLL.mp4', import.meta.url).href
-</script>
