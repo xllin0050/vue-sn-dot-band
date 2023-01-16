dc4292bfa0dab7787f6942ea4cc3b8250974b002
Carl Lin <xllin@tanzi>
2023-01-16 20:59:55 +0800

    add: gig note

--- src/components/GigInfoModal.vue
+++ src/components/GigInfoModal.vue
@@ -6,7 +6,7 @@
   >
     <div
       id="modal"
-      class="relative flex w-3/4 flex-col items-center justify-between rounded-md bg-neutral-50 p-4 font-redhat uppercase  md:w-1/3 md:p-12"
+      class="relative flex w-3/4 flex-col  justify-between rounded-md bg-neutral-50 p-4 font-redhat uppercase  md:w-1/3 md:p-12"
     >
       <div
         class="absolute top-2 right-2 rounded bg-neutral-50 p-2 text-base shadow-lg hover:bg-neutral-100"
@@ -24,22 +24,23 @@
           class="mx-auto w-full"
           @load="show = false"
         />
-        <div v-if="show" class="h-[300px] w-full text-center">
+        <div v-if="show" class="h-[300px] w-full text-center pt-8">
           wait for poster loading or nothing at all
         </div>
       </div>
       <div class="flex flex-col p-4 text-sm lg:text-base">
         <p class="pt-6">{{ props.gigInfoData.show_time }}</p>
         <p class="pt-6">{{ props.gigInfoData.venue }}</p>
+        <p class="pt-6 text-xs text-gray-500 normal-case">{{ props.gigInfoData.note }}</p>
       </div>
       <div class="pt-8">
         <div
-          class="rounded bg-neutral-50 p-2 text-base shadow hover:bg-neutral-100"
+          class="rounded bg-neutral-100 p-2 text-base shadow hover:bg-neutral-100"
         >
           <a
             :href="props.gigInfoData.event_url"
             target="_blank"
-            class="flex items-center"
+            class="flex justify-center items-center"
             >info
             <span
               class="iconify ml-1"
